5f3a52dcd6833631080056c267532d49dfdff316
zemmouri salah <sszemmouri@gmail.com>
2024-07-22 12:19:03 +0200

    [data] add data package with user data module

---
+++ data/user.py
@@ -0,0 +1,16 @@
+class User:
+
+    _user_pretty_name_to_user_password = {
+        "standard user": ["standard_user", "secret_sauce"]
+    }
+
+    def __init__(self, user_pretty_name):
+        try:
+            self.name = self._user_pretty_name_to_user_password[
+                user_pretty_name][0]
+            self.password = self._user_pretty_name_to_user_password[
+                user_pretty_name][1]
+        except KeyError:
+            raise KeyError(
+                f'User {user_pretty_name} does not exist.'
+            )
