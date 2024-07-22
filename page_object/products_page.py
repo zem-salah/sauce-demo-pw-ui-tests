680b323f37e835f04110cfb0d2bc18613040ba09
zemmouri salah <sszemmouri@gmail.com>
2024-07-22 17:42:14 +0200

    [page_object] implement low level add product to cart click

--- page_object/products_page.py
+++ page_object/products_page.py
@@ -1,3 +1,4 @@
+from data.products import ProductsData
 from page_object.base_page import BasePage
 
 
@@ -5,6 +6,14 @@ class ProductsPage(BasePage):
 
     def __init__(self, page):
         super().__init__(page)
+        self.add_product_to_cart_locator_prefix = 'add-to-cart-{:s}'
 
     def is_visible(self):
         return True
+
+    def add_product_to_cart(self, product_pretty_name):
+        self.page.get_by_test_id(
+            self.add_product_to_cart_locator_prefix.format(
+                ProductsData.get_product_locator_value(product_pretty_name)
+            )
+        ).click()
