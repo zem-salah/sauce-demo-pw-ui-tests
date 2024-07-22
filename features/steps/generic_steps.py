b9ce854169a5773301fa33a48914a2d9fa8f3a2f
zemmouri salah <sszemmouri@gmail.com>
2024-07-22 12:29:33 +0200

    [steps] add step to check user is on a page

--- features/steps/generic_steps.py
+++ features/steps/generic_steps.py
@@ -1,5 +1,6 @@
 from behave import given, then
 
+from Assertion.assertion import AssertThat
 from data.user import User
 from page_object.page_factory import PageFactory
 
@@ -8,3 +9,9 @@ from page_object.page_factory import PageFactory
 def navigate_to_form(context, user_role, form_pretty_name):
     context.current_user = User(user_role)
     PageFactory(context.page)(form_pretty_name).navigate()
+
+
+@then('he should be on "{page_pretty_name}" page')
+def assert_page_is_visible(context, page_pretty_name):
+    AssertThat.Page(page_pretty_name).is_visible()
+    AssertThat.Page(page_pretty_name).is_current_page()
