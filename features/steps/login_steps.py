89cc9775dc152da102874ba1b4474f7f7de55f37
zemmouri salah <sszemmouri@gmail.com>
2024-07-25 15:39:31 +0200

    [steps] rename login step implementaiton method

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -5,6 +5,6 @@ from data.user import User
 
 
 @when('the user logs in as "{user_role}"')
-def impl(context, user_role):
+def login_as_user(context, user_role):
     context.current_user = User(user_role)
     context.current_page = Login.login_as(context.current_user)
