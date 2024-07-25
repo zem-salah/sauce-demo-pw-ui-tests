dafcd7d0c27b104171fad8058db9a4c9bcfe4e3d
zemmouri salah <sszemmouri@gmail.com>
2024-07-25 15:41:31 +0200

    [steps] add cart step module and first step to assert that cart contain X number of product(s)

---
+++ features/steps/cart_steps.py
@@ -0,0 +1,8 @@
+from behave import then
+
+from Assertion.assertion import AssertThat, Cart
+
+
+@then('the cart should contain "{expected_number_of_products}" product')
+def assert_cart_contain_product(context, expected_number_of_products):
+    AssertThat(Cart).contains_number_of_product(expected_number_of_products)
