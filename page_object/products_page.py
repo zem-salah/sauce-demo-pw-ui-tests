64cd9fb30277e139d191fb8d0e6595a88f993dbf
zemmouri salah <sszemmouri@gmail.com>
2024-07-25 18:14:33 +0200

    [page_objects] add remove button locator to product page object

--- page_object/products_page.py
+++ page_object/products_page.py
@@ -1,3 +1,5 @@
+from playwright.sync_api import Locator
+
 from data.products import Product
 from page_object.base_page import BasePage
 
@@ -7,13 +9,24 @@ class ProductsPage(BasePage):
     def __init__(self, page):
         super().__init__(page)
         self.add_product_to_cart_locator_prefix = 'add-to-cart-{:s}'
+        self.remove_product_from_cart_locator_prefix = 'remove-{:s}'
 
     def is_visible(self):
         return True
 
-    def add_product_to_cart(self, product_name) -> None:
-        self.page.get_by_test_id(
+    def add_to_cart(self, product_name) -> Locator:
+        return self.page.get_by_test_id(
             self.add_product_to_cart_locator_prefix.format(
                 Product.get_locator_value(product_name)
             )
-        ).click()
+        )
+
+    def add_product_to_cart(self, product_name) -> None:
+        self.add_to_cart(product_name).click()
+
+    def remove(self, product_name) -> Locator:
+        return self.page.get_by_test_id(
+            self.remove_product_from_cart_locator_prefix.format(
+                Product.get_locator_value(product_name)
+            )
+        )
