64e204036901ccdd3d88d3666fa2a2f521a68e36
zemmouri salah <sszemmouri@gmail.com>
2024-07-26 17:18:38 +0200

    [steps] add step to got to the shopping cart via the primary header menu

--- features/steps/cart_steps.py
+++ features/steps/cart_steps.py
@@ -1,6 +1,12 @@
-from behave import then
+from behave import then, when
 
 from Assertion.assertion import AssertThat, Cart
+from Actions.navigation_actions import Navigate
+
+
+@when('he goes to the shopping cart')
+def navigate_to_cart(context):
+    context.current_page = Navigate.to_shopping_cart()
 
 
 @then('the shopping cart is empty')
