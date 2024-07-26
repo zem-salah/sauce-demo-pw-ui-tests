6365932596ec32aecc7e971e67e529631122a27e
zemmouri salah <sszemmouri@gmail.com>
2024-07-26 17:21:45 +0200

    [page_objects] add cart link and access_cart method in primary header page object

--- page_object/primary_header.py
+++ page_object/primary_header.py
@@ -6,3 +6,9 @@ class PrimaryHeader(BasePage):
     def __init__(self, page):
         super().__init__(page)
         self.cart_badge = page.get_by_test_id('shopping-cart-badge')
+        self.cart_link = page.get_by_test_id('shopping-cart-link')
+
+    def access_cart(self):
+        from page_object.page_factory import PageFactory
+        self.cart_link.click()
+        return PageFactory(self.page)('shopping cart')
