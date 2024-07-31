7e2e7f52f534ce4ce0dd8e407e0b0296dd42785a
zemmouri salah <sszemmouri@gmail.com>
2024-07-31 14:29:32 +0200

    [page_object] add methods to get list of products in cart and a quantity of a product in cart

--- page_object/cart_page.py
+++ page_object/cart_page.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import expect
 
+from data.products import Product
 from page_object.base_page import BasePage
 
 
@@ -13,3 +14,12 @@ class CartPage(BasePage):
         expect(self.page.get_by_test_id('continue-shopping')).to_be_visible()
         expect(self.page.get_by_test_id('checkout')).to_be_visible()
         return True
+
+    def get_products(self) -> list[Product]:
+        cart_items = self.page.get_by_test_id('cart-list') \
+            .get_by_test_id('inventory-item-name').all_inner_texts()
+        return [Product(cart_item) for cart_item in cart_items]
+
+    def get_quantity_of_product(self, product: Product) -> int:
+        return self.page.get_by_test_id('inventory-item').filter(
+            has_text=product.name).get_by_test_id('item-quantity').inner_text()
