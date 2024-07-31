2e5c9b014cbb136c190a15c0ca1f433bf670250f
zemmouri salah <sszemmouri@gmail.com>
2024-07-31 14:56:06 +0200

    [actions] implement proceed to checkout action

--- Actions/navigation_actions.py
+++ Actions/navigation_actions.py
@@ -1,5 +1,6 @@
 from page_object.page_factory import PageFactory
 from page_object.cart_page import CartPage
+from page_object.checkout_form import CheckoutForm
 
 
 class Navigate:
@@ -12,7 +13,17 @@ class Navigate:
     def set_page(cls, page):
         cls.page = page
         cls.primary_header = PageFactory(cls.page)('primary header')
+        cls.cart_page = PageFactory(cls.page)('shopping cart')
 
     @classmethod
     def to_shopping_cart(cls) -> CartPage:
         return cls.primary_header.access_cart()
+
+    @classmethod
+    def proceed_to_checkout(cls) -> CheckoutForm:
+        """
+        In cart page, we click on checkout button to go to the checkout
+        information form
+        :return: None
+        """
+        return cls.cart_page.checkout()
