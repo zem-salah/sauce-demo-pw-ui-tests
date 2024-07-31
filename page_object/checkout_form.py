ea25e3d6352b71c7cddbd4c69b7a5a1f504a9bf5
zemmouri salah <sszemmouri@gmail.com>
2024-07-31 17:18:52 +0200

    [page_objects] add fields locators to CheckoutForm page object

--- page_object/checkout_form.py
+++ page_object/checkout_form.py
@@ -5,6 +5,12 @@ class CheckoutForm(BasePage):
 
     def __init__(self, page):
         super().__init__(page)
+        self.page_elements_pretty_name_to_locator = {
+            'first name': page.get_by_test_id('firstName'),
+            'last name': page.get_by_test_id('lastName'),
+            'postal code': page.get_by_test_id('postalCode'),
+            'continue': page.get_by_test_id('continue'),
+        }
 
     def is_visible(self):
         return True
