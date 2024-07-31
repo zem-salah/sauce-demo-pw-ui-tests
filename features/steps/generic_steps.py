93082f1ec05cc69e6e5be13ca93bcf36d8e03f58
zemmouri salah <sszemmouri@gmail.com>
2024-07-31 17:17:16 +0200

    [steps] add step for user to fill a form from field and values coming from table

--- features/steps/generic_steps.py
+++ features/steps/generic_steps.py
@@ -1,5 +1,6 @@
-from behave import given, then
+from behave import given, then, when
 
+from Actions.form_actions import Form
 from Assertion.assertion import AssertThat, Page
 from page_object.page_factory import PageFactory
 
@@ -7,6 +8,25 @@ from page_object.page_factory import PageFactory
 @given('sauce demo login form is visible')
 def navigate_to_login_form(context):
     PageFactory(context.page)('login').navigate()
+
+
+@when('he fills')
+def user_fills_form(context):
+    """
+    Generic step to fill a form from a table composed of two columns
+    '|field|value|'
+
+    Each row in this table will be used to fill one field in the form.
+
+    'field' column, must contain pretty names of the fields to fill.
+    Exemple :
+        |field      |value        |
+        |user name  | new user    |
+        |address    | foo         |
+        |email      | bar@foo.com |
+        ...
+    """
+    Form(context.current_page).fill_from_table(context.table)
 
 
 @then('he should be on "{page_pretty_name}" page')
