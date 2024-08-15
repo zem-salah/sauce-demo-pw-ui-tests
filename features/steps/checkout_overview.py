6c1c399ea2098410f032e2f88994cc7cc0819fae
zemmouri salah <sszemmouri@gmail.com>
2024-08-15 10:56:21 +0200

    [steps] add step to navigate from checkout overview to checkout complete page

---
+++ features/steps/checkout_overview.py
@@ -0,0 +1,8 @@
+from behave import when
+
+from Actions.navigation_actions import Navigate
+
+
+@when('the user completes the checkout')
+def navigate_to_checkout_complete(context):
+    context.current_page = Navigate.to_checkout_complete()
