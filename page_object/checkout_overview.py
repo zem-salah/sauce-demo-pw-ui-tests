68400d76f979686e7c2b0b29db2b946ebc731b62
zemmouri salah <sszemmouri@gmail.com>
2024-08-15 10:58:17 +0200

    [page_objects] add access_to_checkout_complete page object method in checkout_overview page object

--- page_object/checkout_overview.py
+++ page_object/checkout_overview.py
@@ -5,6 +5,12 @@ class CheckoutOverview(BasePage):
 
     def __init__(self, page):
         super().__init__(page)
+        self.finish_btn = page.get_by_test_id('finish')
 
     def is_visible(self):
         return True
+
+    def access_checkout_complete(self):
+        from page_object.page_factory import PageFactory
+        self.finish_btn.click()
+        return PageFactory(self.page)('checkout complete')
